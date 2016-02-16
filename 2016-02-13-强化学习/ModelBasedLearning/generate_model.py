b403029fb803496c956bb3b0f724c3c6f6245a39
lichen <lichenarthurml@gmail.com>
2016-02-16 11:35:40 +0800

    完成model based learning, policy iteration

--- 2016-02-13-强化学习/ModelBasedLearning/generate_model.py
+++ 2016-02-13-强化学习/ModelBasedLearning/generate_model.py
@@ -5,13 +5,15 @@ from config import *
 from File.FilePath import *
 
 print 'config, X count: %d, A count: %d' %(X_count, A_count)
-
 P = np.random.rand(X_count, X_count, A_count)
 P = P / np.expand_dims(np.sum(P, axis=2), axis=2)
 print P
 print ''
 
-R = np.random.randint(5, size=(X_count, X_count, A_count)) + 1
+R = np.random.randint(5, size=(X_count, X_count, A_count)) + 1.0
+R[0, 0, 0] += 5
+R[1, 1, 1] += 10
+R[2, 2, 2] += 15
 print R
 print ''
 
--- 2016-02-13-强化学习/ModelBasedLearning/policy_iteration.py
+++ 2016-02-13-强化学习/ModelBasedLearning/policy_iteration.py
@@ -15,21 +15,48 @@ Pi = np.ones((X_count, A_count)) / float(A_count)
 
 def newValue():
 	Vtmp = np.zeros(X_count)
-	for i in xrange(1, T+1):
-		for x_from_index in xrange(X_count):
+	for step in xrange(1, T+1):
+		Vtmptmp = np.zeros(X_count)
+		for x_from_index in range(X_count):
 			value = 0.0
 			for a_index in range(A_count):
 				for x_to_index in range(X_count):
 					possiblity_action = Pi[x_from_index, a_index]
 					possiblity_transfer = P[x_from_index, x_to_index, a_index]
 					reward = R[x_from_index, x_to_index, a_index]
-					
+					value += possiblity_action * possiblity_transfer * (reward / float(step) + ((step - 1) / float(step)) * Vtmp[x_to_index])
+			Vtmptmp[x_from_index] = value
+		Vtmp = Vtmptmp
+	return Vtmp
 
 def newPolicy():
-	pass
+	Q = np.zeros((X_count, A_count))
+	for x_from_index in range(X_count):
+		for a_index in range(A_count):
+			value = 0.0
+			for x_to_index in range(X_count):
+				possiblity_transfer = P[x_from_index, x_to_index, a_index]
+				reward = R[x_from_index, x_to_index, a_index]
+				value += possiblity_transfer * (reward / float(T+1) + T * V[x_to_index] / float(T+1))
+			Q[x_from_index, a_index] = value
+	QMaxIndex = np.argmax(Q, axis=1)
+	Pitmp = np.zeros((X_count, A_count))
+	for i in range(X_count):
+		Pitmp[i, QMaxIndex[i]] = 1.0
+	return Pitmp
 
-print V
-print Pi
+def purePolicy():
+	return np.argmax(Pi, axis=1)
+
+for i in range(100):
+	Piclear = purePolicy()
+	print Piclear
+	V = newValue()
+	Pi = newPolicy()
+	if np.array_equal(Piclear, purePolicy()):
+		break
+
+
 
 
 
